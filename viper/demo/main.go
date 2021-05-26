e3430c8a4a5502f449a32a344d022b6130de891d
TianXing Wang <2576279414@qq.com>
2021-05-26 17:09:07 +0800

    完善了viper库的demo

--- viper/demo/main.go
+++ viper/demo/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	viper.AddConfigPath("dsd")
+	viper.SetConfigName("userInfo")
+	viper.AddConfigPath("./demo/config")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(viper.Get("User1.Username"))
+	// 写入到一个新文件中
+	viper.Set("User1.Age",30)
+	viper.SafeWriteConfigAs("./demo/config/newUserInfo.yaml")
+
+	// 要在WatchConfig前调用这个方法
+	// 一次改变会调用两次这个函数，这是个viper自己的bug
+	viper.OnConfigChange(func(in fsnotify.Event){
+		fmt.Println("配置改变了")
+	})
+	viper.WatchConfig()
+	for  {
+
+	}
+	return
 }
--- viper/go.mod
+++ viper/go.mod
@@ -2,4 +2,7 @@ module hello/WeeklyLearning/viper
 
 go 1.16
 
-require github.com/spf13/viper v1.7.1 // indirect
+require (
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/spf13/viper v1.7.1
+)
